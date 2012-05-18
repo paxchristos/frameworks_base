33811cd7900f8e7b4d1f43ea9bd0a61f9743e3ac
paxchristos <nielson.peter@gmail.com>
2012-05-18 16:50:20 -0500

    Revert "Removed due to compiling issues"
    This reverts commit a9ca8f751eaa7456a7994a213e704bb4a19f9758.

--- core/jni/android_view_GLES20Canvas.cpp
+++ core/jni/android_view_GLES20Canvas.cpp
@@ -627,9 +627,9 @@ static void android_view_GLES20Canvas_resetDisplayListRenderer(JNIEnv* env,
     renderer->reset();
 }
 
-/*static void android_view_GLES20Canvas_destroyDisplayList(JNIEnv* env,
+static void android_view_GLES20Canvas_destroyDisplayList(JNIEnv* env,
         jobject clazz, DisplayList* displayList) {
-    DisplayList::destroyDisplayListDeferred(displayList); removed */
+    DisplayList::destroyDisplayListDeferred(displayList);
 }
 
 static bool android_view_GLES20Canvas_drawDisplayList(JNIEnv* env,
