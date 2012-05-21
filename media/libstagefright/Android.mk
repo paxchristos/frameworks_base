ffedb4065f844c09d66c0635ffbe20b8a5978ab1
paxchristos <nielson.peter@gmail.com>
2012-05-21 02:55:48 -0500

    more CM9 merges

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -79,11 +79,13 @@ LOCAL_SRC_FILES:=                         \
         XINGSeeker.cpp                    \
         avc_utils.cpp                     \
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-        LOCAL_SRC_FILES += ExtendedExtractor.cpp
-        LOCAL_SRC_FILES += ExtendedWriter.cpp
-	LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
-endif
+#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+#        LOCAL_SRC_FILES += ExtendedWriter.cpp
+#        LOCAL_SRC_FILES += ExtendedExtractor.cpp
+#	LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
+#endif
+# removed due to it being a not generating the correct libs when compiling.
+
 
 LOCAL_C_INCLUDES+= \
 	$(JNI_H_INCLUDE) \
